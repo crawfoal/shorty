06a1a5afd7b335a6a4236c812e6bb09640c1bffb
Amanda Dolan <amandacrawfordalc@gmail.com>
2022-01-07 09:15:50 -0700

    Adds plug and defines fallback match

--- lib/shorty/application.ex
+++ lib/shorty/application.ex
@@ -8,8 +8,7 @@ defmodule Shorty.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Shorty.Worker.start_link(arg)
-      # {Shorty.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: Shorty.Router, options: [port: 4001]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/shorty/router.ex
@@ -0,0 +1,10 @@
+defmodule Shorty.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  match _ do
+    send_resp(conn, 404, "oops")
+  end
+end
